2f3b9a47911201f6e4942c102a31aee2a3715427
GBTC408014UR <GBTC408014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-12 10:53:49 -0400

    User entry login and page and process to another html page

---
+++ SpringBoot_201/src/main/java/com/example/sillygoose/SpringBoot201appApplication.java
@@ -0,0 +1,13 @@
+package com.example.sillygoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot201appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot201appApplication.class, args);
+	}
+
+}
